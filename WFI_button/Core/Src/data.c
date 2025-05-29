83b7af6a0841274d415acae9d42d049b03923bff
talursoumya <talursoumya@gmail.com>
2025-05-29 16:02:35 +0530

    Initial commit

---
+++ WFI_button/Core/Src/data.c
@@ -0,0 +1,14 @@
+/*
+ * data.c
+ *
+ *  Created on: Mar 24, 2025
+ *      Author: talur
+ */
+
+#include "main.h"
+
+//some random generated text
+
+char some_data[] = "We are testing SLEEPONEXIT feature\r\n";
+
+
